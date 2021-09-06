6d1d24ed90df24da97a6f0b20206ffc4001e6b46
sara <smhumphries@hotmail.co.uk>
2021-09-06 13:16:46 +0100

    writes failling tests

--- models/ANDigital_task.js
+++ models/ANDigital_task.js
@@ -1,1 +1,14 @@
+const { PassThrough } = require("stream");
 
+const Solution = function(input) {
+    this.input = input;
+
+    Solution.prototype.execute = function() {
+        return 'pass';
+    };
+
+    
+
+};
+
+module.exports = Solution;
--- specs/tests.js
+++ specs/tests.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const Solution = require('../models/ANDigital_task.js')
+const Solution = require('../models/ANDigital_task.js');
 
 
 describe('Solution', function() {
@@ -10,6 +10,12 @@ describe('Solution', function() {
 
     it("should output '632, 623, 362, 326, 263, 236' for '326'", function() {
         const actual = solution.execute('326');
+        const expected = ['632','623','362','326','263','236']
+        assert.deepStrictEqual(actual, expected);
+    });
+
+    it("should output '632, 623, 362, 326, 263, 236' for 'A 3B2 C6D'", function() {
+        const actual = solution.execute('A 3B2 C6D');
         const expected = ['632','623','362','326','263','236']
         assert.deepStrictEqual(actual, expected);
     });
