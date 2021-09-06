3f79c018d17a54cf0a03200530391c661ded74ef
sara <smhumphries@hotmail.co.uk>
2021-09-06 14:04:01 +0100

    returns solutions for 34 first test

--- models/ANDigital_task.js
+++ models/ANDigital_task.js
@@ -1,13 +1,18 @@
 const { PassThrough } = require("stream");
 
-const Solution = function(input) {
-    this.input = input;
-
-    Solution.prototype.execute = function() {
-        return 'pass';
-    };
-
+const Solution = function() {
     
+
+    Solution.prototype.execute = function(input) {
+        let solutions = '';
+        const first_digit = input.split("")[0];
+        const second_digit = input.split("")[1];
+        const solution1 = second_digit + first_digit;
+        solutions += solution1 + ', ';
+        const solution2 = first_digit + second_digit;
+        solutions += solution2;
+        return solutions;
+    };
 
 };
 
--- specs/tests.js
+++ specs/tests.js
@@ -8,17 +8,23 @@ describe('Solution', function() {
         solution = new Solution();
     });
 
-    it("should output '632, 623, 362, 326, 263, 236' for '326'", function() {
-        const actual = solution.execute('326');
-        const expected = ['632','623','362','326','263','236']
-        assert.deepStrictEqual(actual, expected);
-    });
+    it("returns solutions for 2 digit number", function() {
+        const actual = solution.execute('34');
+        const expected = '43, 34';
+        assert.deepStrictEqual(actual, expected);;
+    }); 
 
-    it("should output '632, 623, 362, 326, 263, 236' for 'A 3B2 C6D'", function() {
-        const actual = solution.execute('A 3B2 C6D');
-        const expected = ['632','623','362','326','263','236']
-        assert.deepStrictEqual(actual, expected);
-    });
+    // it("should output '632, 623, 362, 326, 263, 236' for '326'", function() {
+    //     const actual = solution.execute('326');
+    //     const expected = '632,623,362,326,263,236';
+    //     assert.deepStrictEqual(actual, expected);
+    // });
+
+    // it("should output '632, 623, 362, 326, 263, 236' for 'A 3B2 C6D'", function() {
+    //     const actual = solution.execute('A 3B2 C6D');
+    //     const expected = '632,623,362,326,263,236';
+    //     assert.deepStrictEqual(actual, expected);
+    // });
 
 
 })
