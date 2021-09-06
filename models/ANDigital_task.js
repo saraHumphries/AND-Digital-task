6ec927b3334e1072a765ac33db4b8dc5616adb1d
sara <smhumphries@hotmail.co.uk>
2021-09-06 15:25:35 +0100

    refacors 2 and 3 digit solution

--- models/ANDigital_task.js
+++ models/ANDigital_task.js
@@ -5,24 +5,13 @@ const Solution = function() {
 
     Solution.prototype.execute = function(input) {
 
+        const ordered_digits = input.split("").sort().reverse();
+
         if (input.length == 2) {
-            const ordered_digits = input.split("").sort().reverse();
             return this.dealWith2Digits(ordered_digits);
         }
-        else {
-            const solutions = [];
-        const ordered_digits = input.split("").sort().reverse();
-        for (const digit of ordered_digits) {
-            const indexOfDigit = ordered_digits.indexOf(digit);
-            const other_digits = [...ordered_digits]
-            other_digits.splice(indexOfDigit, 1);
-            const twoDigits = this.dealWith2Digits(other_digits);
-            for (result of twoDigits) {
-                const solution = digit.concat(result);
-                solutions.push(solution);
-            };
-        };
-        return solutions;
+        else if (input.length == 3) {
+        return this.dealWith3Digits(ordered_digits);
         };
     };
 
@@ -35,6 +24,21 @@ const Solution = function() {
         solutions.push(solution1);
         const solution2 = smallest_digit + largest_digit;
         solutions.push(solution2);
+        return solutions;
+    };
+
+    Solution.prototype.dealWith3Digits = function(threeDigitArray) {
+        const solutions = [];
+        for (const digit of threeDigitArray) {
+            const indexOfDigit = threeDigitArray.indexOf(digit);
+            const other_digits = [...threeDigitArray]
+            other_digits.splice(indexOfDigit, 1);
+            const twoDigits = this.dealWith2Digits(other_digits);
+            for (result of twoDigits) {
+                const solution = digit.concat(result);
+                solutions.push(solution);
+            };
+        };
         return solutions;
     };
 
